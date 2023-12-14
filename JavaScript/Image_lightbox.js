9a5759c0837590f20651c7714d7841de3e4cff74
ortis88 <caesar0718@gmail.com>
2023-12-14 15:44:36 +0800

    rwd

---
+++ JavaScript/Image_lightbox.js
@@ -0,0 +1,16 @@
+// function showLightBox(){
+//     document.getElementById("preview-wrap").style.display="block";
+// }
+// function closeLightBox(){
+//     document.getElementById("preview-wrap").style.display="none";
+// }
+
+// function contrlBox(){
+//     document.getElementById("preview-wrap").onclick=
+//     closeLightBox;
+
+//     document.getElementById("open-preview1").onclick= showLightBox;
+// }
+// window.addEventListener("load", contrlBox, false);
+
+
