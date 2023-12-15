b55a421fb67bde824caa5b8a168e1a6584cbd207
ortis88 <caesar0718@gmail.com>
2023-12-16 01:06:44 +0800

    rwd finish

--- JavaScript/Image_lightbox.js
+++ JavaScript/Image_lightbox.js
@@ -1,16 +1,17 @@
-// function showLightBox(){
-//     document.getElementById("preview-wrap").style.display="block";
-// }
-// function closeLightBox(){
-//     document.getElementById("preview-wrap").style.display="none";
-// }
+function showLightBox(){
+    document.getElementById("preview-wrap").style.display="block";
+}
+function closeLightBox(){
+    document.getElementById("preview-wrap").style.display="none";
+}
 
-// function contrlBox(){
-//     document.getElementById("preview-wrap").onclick=
-//     closeLightBox;
+function contrlBox(){
+    document.getElementById("preview-wrap").onclick=
+    closeLightBox;
 
-//     document.getElementById("open-preview1").onclick= showLightBox;
-// }
-// window.addEventListener("load", contrlBox, false);
+    document.getElementById("open-preview1").onclick= showLightBox;
+}
+window.addEventListener("load", contrlBox, false);
+
 
 
---
+++ JavaScript/slick.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $('.items-row').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    });
+  });
