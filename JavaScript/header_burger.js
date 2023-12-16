6106f25545e5ad479235115262f4032bc2164894
ortis88 <caesar0718@gmail.com>
2023-12-16 12:07:28 +0800

    header animation

--- JavaScript/header_burger.js
+++ JavaScript/header_burger.js
@@ -1,17 +1,12 @@
 window.addEventListener("load", function() {
     document.getElementById("header-switch").onclick = function() {
-        let menu = document.getElementById("nav-menu");
+        let menu = document.getElementById("header-space");
         if(menu.classList.contains("hidden")){
             menu.classList.remove("hidden");
         } else {
             menu.classList.add("hidden");
         }
-        };	
-    
-        document.getElementById("header-switch").addEventListener('click', function() {
-            this.classList.toggle('is-active');
-          });
-          
+        };	      
 
 }, false);
 
